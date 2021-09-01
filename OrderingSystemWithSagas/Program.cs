dd7c9840b77d4fc4e66730ad2a8ad1685757f685
frma <frma@hesehus.dk>
2021-09-01 17:13:51 +0200

    fuck around with commandline

--- OrderingSystemWithSagas/Program.cs
+++ OrderingSystemWithSagas/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Windows.Input;
+using McMaster.Extensions.CommandLineUtils;
 
 namespace OrderingSystemWithSagas
 {
+    interface ICommandable
+    {
+        void OnExecute(IConsole console);
+    }
+    
+    [Command(Name = "Entry")]
     class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        private int OnExecute(CommandLineApplication app, IConsole console)
         {
-            Console.WriteLine("Hello World!");
+            console.WriteLine("You must specify at a subcommand.");
+            app.ShowHelp();
+            return 1;
+        } 
+        
+        [Command("orders", "Manage Orders")]
+        private class Order
+        {
+            private void OnExecute(IConsole console)
+            {
+                Console.WriteLine("hello world");
+            }
         }
     }
 }
